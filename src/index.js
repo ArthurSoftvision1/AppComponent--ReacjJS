0968aeee43ced3a43c55b0dc63f6ee4961d3863f
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-17 10:15:31 +0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; // import react
+import ReactDOM from 'react-dom'; //import react library
+import App from './App'; // import App component
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
