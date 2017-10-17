50e8d863bf335cd47727f0c2948452fd1f01b58d
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-17 12:42:52 +0300

    React Component Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
 import React from 'react'; // import react
+import ReactDOM from 'react-dom';
+
 
 class App extends React.Component {
+
   constructor() {
-    super();
-    this.state = {currentEvent: '__'} // default state
+    super() //to get our context
+    this.state = {val: 0} // set initial state
     this.update = this.update.bind(this)
   }
 
-  update(event) { // update event function
-    this.setState({currentEvent: event.type})
+  update() {
+    this.setState({val: this.state.val + 1}) // increment the state
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  render() {
+    console.log('render');
+    return <button onClick={this.update}>{this.state.val} </button>
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentWillUnMount() {
+    console.log('componentWillUnMount')
+  }
+}
+
+class Wrapper extends React.Component {
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  
+  unMount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
   }
 
   render() {
     return (
       <div>
-        <textarea
-          onKeyPress={this.update} // onKeyPress update the state
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          onDoubleClick={this.update}
-          onTouchStart={this.update}
-          onTouchMove={this.update}
-          onTouchEnd={this.update}
-          cols="30"
-          rows="30" />
-          <h1>{this.state.currentEvent}</h1> 
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unMount.bind(this)}>UnMount</button>
+        <div id="a"> </div>
       </div>
     )
-  } 
+  }
 }
 
-
-export default App // export App component
+export default Wrapper // export Wrapper component
