ca87b2c812e3c79a8bc3e152710cbd5d5e976ac6
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-10-19 13:40:51 +0300

    Rest of the files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'; // import react
 import ReactDOM from 'react-dom'; //import react library
 import App from './App'; // import App component
+// import Map from './map'; // import map
 
 ReactDOM.render(
     <App />,
